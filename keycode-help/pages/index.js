8bc290239ab3555d8de014fd4bb56d0c5d278fe3
Anthony Feaster <5epmgllc@gmail.com>
2024-08-23 22:53:05 -0400

    ideas.txt ideas for the app- add all ideas here

---
+++ keycode-help/pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+      <div>
+        <h1>Welcome to Keycode.help</h1>
+        <p>Your keycode assistance starts here.</p>
+      </div>
+    );
+  }
+  
