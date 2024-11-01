e27978676b1c51c120a0942761a874d6d56baefb
MrGuruApple <apple@MrGuru.local>
2024-11-01 12:24:45 -0400

    Initial setup with environment files and branch structure

--- keycode-help/pages/index.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-    return (
-      <div>
-        <h1>Welcome to Keycode.help</h1>
-        <p>Your keycode assistance starts here.</p>
-      </div>
-    );
-  }
-  
