2f72a28b5de69009796ddf9aa0ecba03122e86cc
nithyamahe <nithya.maheswaran@artsphereinc.org>
2025-01-21 16:39:30 -0500

    Admin Dashboard Update

--- kch-backend/src/main/java/org/rma/kchbackend/controller/VehicleKeycodeController.java
+++ kch-backend/src/main/java/org/rma/kchbackend/controller/VehicleKeycodeController.java
@@ -101,9 +101,11 @@ public class VehicleKeycodeController {
 
         List<Vehicle> pendingRequests = vehicleService.getVehiclesByStatus(user, "PENDING");
         List<Vehicle> fulfilledRequests = vehicleService.getVehiclesByStatus(user, "COMPLETED");
+        List<Vehicle> inProgressRequests = vehicleService.getVehiclesByStatus(user, "INPROGRESS");
 
         Map<String, List<Vehicle>> response = new HashMap<>();
         response.put("pendingRequests", pendingRequests);
+        response.put("inProgressRequests", inProgressRequests);
         response.put("fulfilledRequests", fulfilledRequests);
 
         return ResponseEntity.ok(response);
