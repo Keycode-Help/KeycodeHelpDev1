071faa97e6af76f1186f4a2eb773e27a8f9b7c1e
Norumai01 <johnnydinhnguyen2001@gmail.com>
2025-01-21 02:04:54 -0500

    Changed a bit of Navbar links to stay center when screen size is changed. Removed unused css stylings in app.css. Modified other pages missing elements to stay original to its original style.

--- kch-frontend/src/pages/SubscriptionPage.jsx
+++ kch-frontend/src/pages/SubscriptionPage.jsx
@@ -2,11 +2,22 @@ import React from "react";
 import { useAuth } from "../context/AuthContext";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { blue } from "@mui/material/colors";
 
 function SubscriptionPage() {
   const { token } = useAuth();
   const navigate = useNavigate();
 
+  const containerStyle = {
+    width: '100%',
+    maxWidth: '1200px',
+    margin: '2rem auto',
+    padding: '2rem',
+    backgroundColor: '#161b22',
+    borderRadius: '12px',
+    boxShadow: '0px 5px 15px rgba(0, 0, 0, 0.4)'
+  };
+  
   const handleAddSubscription = (tier) => {
     const subscription = {
       tier: tier,
@@ -41,8 +52,8 @@ function SubscriptionPage() {
 
   return (
     <div className="wrapper-sub">
-      <div className="container subscription-page">
-        <h1>Select a Subscription</h1>
+      <div style={containerStyle} className="subscription-page">
+        <h1 style={{marginBottom: '1rem'}}>Select a Subscription</h1>
         <div className="subscription-options">
           <div className="subscription-option">
             <h2>Base Subscription</h2>
